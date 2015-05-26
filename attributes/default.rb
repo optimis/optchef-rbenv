8b4bd85a0034f1aee287eaacfcb70a3569f4d7cc
Umang Chouhan <uchouhan@optimisdev.com>
2015-05-26 08:14:03 -0700

    Upgrade bundler to 1.9.9.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,4 @@ node.default['ruby_build']['upgrade'] = true
 ruby_version = '1.9.3-p545'
 node.default['rbenv']['rubies'] = [ ruby_version ]
 node.default['rbenv']['global'] = ruby_version
-node.default['rbenv']['gems'][ruby_version] = [{ name: 'ohai' }, { name: 'chef' }, { name: 'bundler' }]
+node.default['rbenv']['gems'][ruby_version] = [{ name: 'ohai' }, { name: 'chef' }, { name: 'bundler', version: '1.9.9' }]
