7f586ec9e7514a793f2eb4b3056fc9cff0845f75
Umang Chouhan <uchouhan@optimisdev.com>
2017-06-04 13:31:23 -0700

    Upgrade bundler version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,9 @@ node.default['ruby_build']['upgrade'] = true
 ruby_version = '2.2.2'
 node.default['rbenv']['rubies'] = [ ruby_version ]
 node.default['rbenv']['global'] = ruby_version
-node.default['rbenv']['gems'][ruby_version] = [{ name: 'ohai' }, { name: 'chef' }, { name: 'bundler', version: '1.10.6' }]
+
+node.default['rbenv']['gems'][ruby_version] = [
+  { name: 'ohai' },
+  { name: 'chef' },
+  { name: 'bundler', version: '1.15.1' }
+]
