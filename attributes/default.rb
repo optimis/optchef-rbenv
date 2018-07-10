e41fc7ac9c00b2f71a58ee65611061b68c5896e9
Umang Chouhan <uchouhan@optimisdev.com>
2018-07-10 11:51:10 -0700

    Update ruby version to 2.2.9.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
 node.default['ruby_build']['upgrade'] = true
 
-ruby_version = '2.3.3'
+ruby_version = '2.2.9'
 node.default['rbenv']['rubies'] = [ ruby_version ]
 node.default['rbenv']['global'] = ruby_version
 
 node.default['rbenv']['gems'][ruby_version] = [
   { name: 'ohai' },
   { name: 'chef' },
-  { name: 'bundler', version: '1.15.1' }
+  { name: 'bundler', version: '1.16.1' }
 ]
